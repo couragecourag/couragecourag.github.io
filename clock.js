85663980b98001c7dd9c79d697a93d4492cc5204
couragecourag <>
2019-02-20 15:57:21 +0900

    test

--- clock.js
+++ clock.js
@@ -37,7 +37,7 @@ function clock() {
     //　HTML: <span id="clock_date">(ココの日付文字列を書き換え)</span>
     document.getElementById("clock_date").innerHTML = y + "/" + mo + "/" + d + " " + w;
     //　HTML: <span id="clock_time">(ココの時刻文字列を書き換え)</span>
-    document.getElementById("clock_time").innerHTML = h + "!" + mi; /* + ":" + s;*/
+    document.getElementById("clock_time").innerHTML = h + ":" + mi; /* + ":" + s;*/
     //　HTML: <div id="clock_frame"> の内部要素のフォントサイズをウインドウサイズの10分の1ピクセルに設定
     //document.getElementById("clock_frame").style.fontSize = window.innerWidth / 10 + "px";
 }
